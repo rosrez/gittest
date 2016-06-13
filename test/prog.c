6a362b30ddc5095b22ac79739a3243661cda5f5b
Oleg Rosowiecki <rosrez@o2.pl>
2016-06-13 18:51:41 +0200

    updated prog.c

--- test/prog.c
+++ test/prog.c
@@ -1,8 +1,20 @@
 #include <stdlib.c>
 #include <stdio.c>
 
+void greeting(char *name)
+{
+    printf("Hello %s\n", name);
+}
+
 int main(int argc, char *argv[])
 {
-	printf("Hello user\n");
+        char name[32];
+
+        if (argv[1])
+            strncpy(name, argv[1], sizeof(name));
+        else
+            strcpy(name, "user");
+       
+        greeting(name);
 	return 0;
 }
